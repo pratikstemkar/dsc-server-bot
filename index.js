224b1e4592d8ce334b8dab122495d6418916c81b
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-10-13 11:56:06 +0530

    Stream Added

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const Discord = require("discord.js")
 require("dotenv").config()
 const client = new Discord.Client()
 client.on("ready", () => {
+  client.user.setPresence({
+       status: "online",
+       activity: {
+        name: 'HacktoberFestPune',
+        type: 'STREAMING',
+        url: 'https://www.youtube.com/watch?v=_EXAftql0Xg'
+        }
+    })
   console.log(`Logged in as ${client.user.tag}!`)
 })
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
-  }
+
+    if (msg.content === "ping") {
+        msg.reply("Pong!")
+    }
 })
+
 client.login(process.env.BOT_TOKEN)
