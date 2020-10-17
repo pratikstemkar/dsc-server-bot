aba1b9865ab7d829cb44bb09a45ffb76aff69cae
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-10-17 07:50:12 +0530

    Changed Message!

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("ready", () => {
   client.user.setPresence({
        status: "online",
        activity: {
-        name: 'JoJo Rabbit ➜ Tonight, 10pm',
+        name: '🎬 JoJo Rabbit ➜ Tonight, 10pm',
         type: 'PLAYING',
         url: 'https://www.youtube.com/watch?v=_EXAftql0Xg'
         }
