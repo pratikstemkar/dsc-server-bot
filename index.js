aa19c259a4931e0f024ca8f9e25305d7035dd8fa
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-11-17 17:48:01 +0530

    Message Changed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("ready", () => {
   client.user.setPresence({
        status: "online",
        activity: {
-        name: 'Hahaha',
+        name: 'Have a Nice Day!',
         type: 'PLAYING',
         url: 'https://www.youtube.com/watch?v=_EXAftql0Xg'
         }
