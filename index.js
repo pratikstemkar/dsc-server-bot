6f31fe2b1a4764393a39377be54337d5b59121c9
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-11-28 23:49:59 +0530

    ch

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("ready", () => {
   client.user.setPresence({
        status: "online",
        activity: {
-        name: '🎥Hachi➜Saturday, 28th Nov 10PM',
+        name: 'Have a Nice Day!',
         type: 'PLAYING',
         url: 'https://www.youtube.com/watch?v=_EXAftql0Xg'
         }
