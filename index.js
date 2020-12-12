83b4aca5911939c93d4303e9cf6aac61895b62a7
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-12-12 09:36:06 +0530

    Change

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("ready", () => {
   client.user.setPresence({
        status: "online",
        activity: {
-        name: 'HungerGames➜Tonight 10PM',
+        name: 'Before Sunrise➜Tonight 10PM',
         type: 'PLAYING',
         url: 'https://www.youtube.com/watch?v=_EXAftql0Xg'
         }
